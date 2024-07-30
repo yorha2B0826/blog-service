2356f651f07e25ea79b086b65b9847e8789ae261
郑明健 <719939143@qq.com>
2024-07-30 09:49:13 +0800

    完成tag的dao层

--- internal/dao/tag.go
+++ internal/dao/tag.go
@@ -1,1 +1,39 @@
 package dao
+
+import (
+	"github.com/go-programming-tour-book/blog-service/internal/model"
+	"github.com/go-programming-tour-book/blog-service/pkg/app"
+)
+
+func (d *Dao) CountTag(name string, state uint8) (int, error) {
+	tag := model.Tag{Name: name, State: state}
+	return tag.Count(d.engine)
+}
+
+func (d *Dao) GetTagList(name string, state uint8, page, pageSize int) ([]*model.Tag, error) {
+	tag := model.Tag{Name: name, State: state}
+	pageOffset := app.GetPageOffset(page, pageSize)
+	return tag.List(d.engine, pageOffset, pageSize)
+}
+
+func (d *Dao) CreateTag(name string, state uint8, createdBy string) error {
+	tag := model.Tag{
+		Name:  name,
+		State: state,
+		Model: &model.Model{CreatedBy: createdBy}}
+	return tag.Create(d.engine)
+}
+
+func (d *Dao) UpdateTag(id uint32, name string, state uint8, modifiedBy string) error {
+	tag := model.Tag{
+		Model: &model.Model{ID: id, ModifiedBy: modifiedBy},
+		Name:  name,
+		State: state,
+	}
+	return tag.Update(d.engine)
+}
+
+func (d *Dao) DeleteTag(id uint32) error {
+	tag := model.Tag{Model: &model.Model{ID: id}}
+	return tag.Delete(d.engine)
+}
