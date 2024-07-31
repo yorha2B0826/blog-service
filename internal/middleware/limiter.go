22c1e4b09245fa88a83a2a72ec65d4eaf3181615
郑明健 <719939143@qq.com>
2024-07-31 16:16:10 +0800

    限流控制

--- internal/middleware/limiter.go
+++ internal/middleware/limiter.go
@@ -11,7 +11,7 @@ func RateLimiter(l limiter.LimiterIface) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		key := l.Key(c)
 		if bucket, ok := l.GetBucket(key); ok {
-			count := bucket.TakeAvailable(l)
+			count := bucket.TakeAvailable(1)
 			if count == 0 {
 				response := app.NewResponse(c)
 				response.ToErrorResponse(errcode.TooManyRequests)
